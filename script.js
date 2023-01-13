b59c42a24c06fb74109e036e60e459f32e8ce9ff
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-01-13 16:01:07 -0500

    f

--- script.js
+++ script.js
@@ -10,26 +10,18 @@ function black_nav(){
     document.getElementById("products_nav").className="text_white";
     document.getElementById("about_nav").className="text_white";
     document.getElementById("nav_title").className="text_white";
+    var image = document.getElementById("img_pro_nav").src="audio_tin.png"
 }
 function white_nav(){
     document.getElementById("nav").className="white";
     document.getElementById("products_nav").className="text_black";
     document.getElementById("about_nav").className="text_black";
     document.getElementById("nav_title").className="text_black";
-    var image = document.getElementById("img_pro_nav").src="img/audio_tin.png"
+    var image = document.getElementById("img_pro_nav").src="audio_tin.png"
 }
 
-var image = document.getElementById("img_pro_nav")
-if ([prod_name == "Phone_1"]){
-    image.src="img/phone1_tin.png";
-}else if ([prod_name == "audio"]){
-    image.src="img/audio_tin.png";
-}else if ([prod_name == "ear_stick"]){
-    image.src="img/earstick_tin.png";
-}else if ([prod_name == "ear_1"]){
-    image.src="img/ear1_tin.png";
-}else if ([prod_name == "power_45w"]){
-    image.src="img/power45_tin.png";
-}else{
-    image.src="img/audio_tin.png"
-};
+function img_changer(prod_name){
+    var image = document.getElementById("img_pro_nav")
+    var db = { "Phone_1": "img/phone1_tin.png","audio": "img/audio_tin.png","ear_stick": "img/earstick_tin.png","ear_1": "img/ear1_tin.png","power_45w": "img/power45_tin.png"}
+    image.src=db[prod_name];
+}
