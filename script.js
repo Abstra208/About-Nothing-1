42d154b3309656edbbb6249ff2754b14ebfc4214
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-01-13 15:58:25 -0500

    prod_name

--- script.js
+++ script.js
@@ -21,6 +21,17 @@ function white_nav(){
 
 function img_changer(prod_name){
     var image = document.getElementById("img_pro_nav")
-    var db = { "Phone_1": "img/phone1_tin.png","audio": "img/audio_tin.png","ear_stick": "img/earstick_tin.png","ear_1": "img/ear1_tin.png","power_45w": "img/power45_tin.png"}
-    image.src=db[prod_name];
-}
+    if ([prod_name == "Phone_1"]){
+        image.src="img/phone1_tin.png";
+    }else if ([prod_name == "audio"]){
+        image.src="img/audio_tin.png";
+    }else if ([prod_name == "ear_stick"]){
+        image.src="img/earstick_tin.png";
+    }else if ([prod_name == "ear_1"]){
+        image.src="img/ear1_tin.png";
+    }else if ([prod_name == "power_45w"]){
+        image.src="img/power45_tin.png";
+    }else{
+        image.src="img/audio_tin.png"
+    }
+};
