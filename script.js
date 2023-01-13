244e62dd0ede91f55a829cd9203695029fde30c9
WWWCa <111892310+WWWCa@users.noreply.github.com>
2023-01-13 19:58:49 +0000

    fix bug

--- script.js
+++ script.js
@@ -16,12 +16,10 @@ function white_nav(){
     document.getElementById("products_nav").className="text_black";
     document.getElementById("about_nav").className="text_black";
     document.getElementById("nav_title").className="text_black";
-    document.getElementById("prod_img").className="prod_img_none";
 }
 
 function img_changer(prod_name){
     var image = document.getElementById("prod_img");
     var db = { "Phone_1": "img/nothing_phone_1.png","ear_stick": "img/ear_stick.png","ear_1": "img/ear_1.png","power_45w": "img/power_45w.png","black":"img/blacks.png"}
     image.src=db[prod_name];
-    image.className="prod_img"
 }
