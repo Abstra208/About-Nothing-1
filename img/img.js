0eb98cd3a31476aae7957f922a3dc91286f9cc87
WWWCa <111892310+WWWCa@users.noreply.github.com>
2023-01-13 16:38:17 +0000

    test for img changer

---
+++ img/img.js
@@ -0,0 +1,24 @@
+function home(){
+    window.location.assign("/index.html");     
+}
+function tous_les_produits(){
+    window.location="produits.html"
+}
+
+function black_nav(){
+    document.getElementById("nav").className="black";
+    document.getElementById("products_nav").className="text_white";
+    document.getElementById("about_nav").className="text_white";
+    document.getElementById("nav_title").className="text_white";
+}
+function white_nav(){
+    document.getElementById("nav").className="white";
+    document.getElementById("products_nav").className="text_black";
+    document.getElementById("about_nav").className="text_black";
+    document.getElementById("nav_title").className="text_black";
+}
+function img_changer(prod_name){
+    var image = document.getElementById("prod_img");
+    var db = { "Phone_1": "img/nothing_phone_1.png","ear_stick": "img/ear_stick.png","ear_1": "img/ear_1.png","power_45w": "img/power_45w"}
+    image.src=db[prod_name];
+}
