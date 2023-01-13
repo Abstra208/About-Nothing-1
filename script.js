fa76e4d1d39cc4f1999c97f2125bd587228caa04
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-01-13 15:40:52 -0500

    image

--- script.js
+++ script.js
@@ -20,6 +20,6 @@ function white_nav(){
 
 function img_changer(prod_name){
     var image = document.getElementById("img_pro_nav")
-    var db = { "Phone_1": "img/phone1_tin.png","ear_stick": "img/earstick_tin.png","ear_1": "img/ear1_tin.png","power_45w": "img/power45_tin.png"}
+    var db = { "Phone_1": "img/phone1_tin.png","audio": "img/audio_tin.png","ear_stick": "img/earstick_tin.png","ear_1": "img/ear1_tin.png","power_45w": "img/power45_tin.png"}
     image.src=db[prod_name];
 }
