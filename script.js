1b2a08964324c283306932b2206f89aff454c6a8
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-01-16 17:20:44 -0500

    image nav

--- script.js
+++ script.js
@@ -18,10 +18,9 @@ function white_nav(){
     document.getElementById("nav_title").className="text_black";
 }
 
-function img_changer(prod_name,time){
+function img_changer(prod_name){
     var image = document.getElementById("img_pro_nav");
     var db = { "Phone_1": "img/phone1_tin.png","audio": "img/audio_tin.png","ear_stick": "img/earstick_tin.png","ear_1": "img/ear1_tin.png","power_45w": "img/power45_tin.png"}
-    setTimeout [time]
     image.src=db[prod_name];
 }
 
