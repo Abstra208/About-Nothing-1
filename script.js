0fb361611b59b7896069e497162b7a09c9cf9db5
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-01-17 13:56:43 -0500

    try

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ function white_nav(){
     document.getElementById("nav_title").className="text_black";
 }
 
+function nav_screen{
+    var button = document.getElementById("fleche_nav_screen");
+    var nav = document.getElementById("nav_dropdown_screen");
+    nav.style.display="contents";
+}
+
 function img_changer_p(prod_name){
     var image = document.getElementById("img_pro_nav");
     var db = { "Phone_1": "img/phone1_tin.png","audio": "img/audio_tin.png","ear_stick": "img/earstick_tin.png","ear_1": "img/ear1_tin.png","power_45w": "img/power45_tin.png"}
