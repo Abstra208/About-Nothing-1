32d44307f867418376e4206b7996eec1fad5f38c
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-01-16 19:49:56 -0500

    img monde

--- script.js
+++ script.js
@@ -18,9 +18,14 @@ function white_nav(){
     document.getElementById("nav_title").className="text_black";
 }
 
-function img_changer(prod_name){
+function img_changer_p(prod_name){
     var image = document.getElementById("img_pro_nav");
     var db = { "Phone_1": "img/phone1_tin.png","audio": "img/audio_tin.png","ear_stick": "img/earstick_tin.png","ear_1": "img/ear1_tin.png","power_45w": "img/power45_tin.png"}
+    image.src=db[prod_name];
+}
+function img_changer_m(prod_name){
+    var image = document.getElementById("img_mon_nav");
+    var db = { "Carl" : "img/carl-pei_tin.png", "Ludovic" : "img/2profil.png" }
     image.src=db[prod_name];
 }
 
