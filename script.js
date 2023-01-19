afa33601a2f111949e9ad70aabe7b65acec93a26
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-01-19 15:28:01 -0500

    fd

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function tous_les_produits(){
 }
 
 function black_nav(){
-    document.getElementById("nav").className="black";
+    document.getElement("nav").className="black";
     document.getElementById("products_nav").className="text_white";
     document.getElementById("about_nav").className="text_white";
     document.getElementById("nav_title").className="text_white";
 }
 function white_nav(){
-    document.getElementById("nav").className="white";
+    document.getElement("nav").className="white";
     document.getElementById("products_nav").className="text_black";
     document.getElementById("about_nav").className="text_black";
     document.getElementById("nav_title").className="text_black";
