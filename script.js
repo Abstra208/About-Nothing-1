1b15b5b2b243ec4330659a38109b90781c863265
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-01-23 17:22:48 -0500

    nav

--- script.js
+++ script.js
@@ -5,19 +5,6 @@ function tous_les_produits(){
     window.location="produits.html"
 }
 
-function black_nav(){
-    document.getElementById("nav").className="black";
-    document.getElementById("products_nav").className="text_white";
-    document.getElementById("about_nav").className="text_white";
-    document.getElementById("nav_title").className="text_white";
-}
-function white_nav(){
-    document.getElementById("nav").className="white";
-    document.getElementById("products_nav").className="text_black";
-    document.getElementById("about_nav").className="text_black";
-    document.getElementById("nav_title").className="text_black";
-}
-
 function nav_screen(){
     document.getElementById("nav_dropdown_screen").className="visible_nav_screen";
 }
