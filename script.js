00dcc86c66ba1082062408b06211f2c8123fc771
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-02-07 09:12:23 -0500

    asd

--- script.js
+++ script.js
@@ -84,39 +84,16 @@ const checkVisibility = () => {
         }
     }
     if (window.location.pathname === '/produits') {
-        const phone_1 = img_phone1.getBoundingClientRect();
-        const ear_stick = img_earstick.getBoundingClientRect();
-        const ear_1 = img_ear1.getBoundingClientRect();
         const power_45 = img_power45.getBoundingClientRect();
-
-        const phone_1InFrame = (phone_1.top >= 0 && phone_1.bottom >= 0);
-        const ear_stickInFrame = (ear_stick.top >= 0 && ear_stick.bottom <= window.innerHeight);
-        const ear_1InFrame = (ear_1.top >= 0 && ear_1.bottom <= window.innerHeight);
-        const power_45InFrame = (power_45.top >= 0 && power_45.bottom <= window.innerHeight);
-
-        if (phone_1InFrame){
-            console.log('Phone 1 is in frame');
-            document.querySelector('#products_nav').style.color="black";
-            document.querySelector('#about_nav').style.color="black";
-            document.querySelector('#nav_title').style.color="black";
-        }
-        if (ear_stickInFrame){
-            console.log('Ear stick is in frame');
-            document.querySelector('#products_nav').style.color="black";
-            document.querySelector('#about_nav').style.color="black";
-            document.querySelector('#nav_title').style.color="black";
-        }
-        if (ear_1InFrame){
-            console.log('Ear 1 is in frame');
-            document.querySelector('#products_nav').style.color="white";
-            document.querySelector('#about_nav').style.color="white";
-            document.querySelector('#nav_title').style.color="white";
-        }
+        const power_45InFrame = (power_45.top >= 0 && power_45.bottom <= 0);
         if (power_45InFrame){
-            console.log('Power 45 is in frame');
             document.querySelector('#products_nav').style.color="white";
             document.querySelector('#about_nav').style.color="white";
             document.querySelector('#nav_title').style.color="white";
+        }else{
+            document.querySelector('#products_nav').style.color="black";
+            document.querySelector('#about_nav').style.color="black";
+            document.querySelector('#nav_title').style.color="black";
         }
     }
     if (window.location.pathname === '/about'){
