11150c4942005a216c71df013846985606091bdf
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-02-14 07:58:39 -0500

    dfs

--- script.js
+++ script.js
@@ -99,7 +99,3 @@ document.addEventListener("contextmenu", function(event){
 document.addEventListener("click", function(){
     menu.classList.remove("active");
 });
-
-const menu{
-    let produits = document.querySelector("#produits_menu")
-};
