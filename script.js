5820d72fcaa0115c44c49a7b23d364acdf7af431
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-03-11 20:47:44 -0500

    asd

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(function(){
 
 /*VARIABLE*/
 const body = document.querySelector("body");
-const ip = marques.cf;
+
 
 /*navbar*/
 const navTitles = document.querySelectorAll('.nav_title');
@@ -25,7 +25,7 @@ function nav_dropdown_none(){
 
 function navigation(page){
     if (page === "home"){
-        window.location.href= ip;
+        window.location.href="http://marques.cf";
     }
     if (page === "produits"){
         window.location.href="http://marques.cf/produits";
