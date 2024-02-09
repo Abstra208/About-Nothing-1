38174e50d38edad81000e83b4578ccb4077fb7de
Ludovic Morin <LudovicMorin357@hotmail.com>
2024-02-09 03:27:03 +0000

    as

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*LOAD NAVBAR*/
 $(function(){
-    $("header").load("https://about-nothing.netlify.app/xyz/yxt/header.html");
+    $("header").load("/header.html");
 });
 
 /*VARIABLE*/
@@ -12,44 +12,44 @@ const navTitles = document.querySelectorAll('.nav_title');
 
 function navigation(page){
     if (page === "home"){
-        window.location.href="https://marques.cf";
+        window.location.pathname="";
     }
     if (page === "produits"){
-        window.location.href="https://marques.cf/produits";
+        window.location.pathname="/produits";
     }
     if (page === "about"){
-        window.location.href="https://marques.cf/pages/about";
+        window.location.href="/pages/about";
     }
     if (page === "phone_1"){
-        window.location.href="https://marques.cf/produits/phone-1";
+        window.location.href="/produits/phone-1";
     }
     if (page === "audio"){
-        window.location.href="https://marques.cf/pages/audio";
+        window.location.href="/pages/audio";
     }
     if (page === "ear_stick"){
-        window.location.href="https://marques.cf/produits/ear-stick";
+        window.location.href="/produits/ear-stick";
     }
     if (page === "ear_1"){
-        window.location.href="https://marques.cf/produits/ear-1";
+        window.location.href="/produits/ear-1";
     }
     if (page === "ear_2"){
-        window.location.href="https://marques.cf/produits/ear-2"
+        window.location.href="/produits/ear-2"
     }
     if (page === "accessoires"){
-        window.location.href="https://marques.cf/pages/accessoires";
+        window.location.href="/pages/accessoires";
     }
     if (page === "power_45"){
-        window.location.href="https://marques.cf/produits/power-45";
+        window.location.href="/produits/power-45";
     }
 
     if (page === "about"){
-        window.location.href="https://marques.cf/about";
+        window.location.pathname="/about";
     }
     if (page === "CarlP"){
-        window.location.href="https://marques.cf/about/CarlP"
+        window.location.pathname="/about/CarlP"
     }
     if (page === "LudovicM"){
-        window.location.href="https://about.marques.cf"
+        window.location.pathname=""
     }
 }
 
